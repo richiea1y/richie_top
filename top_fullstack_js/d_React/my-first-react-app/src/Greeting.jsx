5f9fc55f9bb98a4b6a8adbdac0e33ab73e04aeef
richiea1y <44350561+richiea1y@users.noreply.github.com>
2025-10-06 21:29:47 +0800

    feat(rendering): Rendering data from arrays

--- top_fullstack_js/d_React/my-first-react-app/src/Greeting.jsx
+++ top_fullstack_js/d_React/my-first-react-app/src/Greeting.jsx
@@ -1,5 +1,21 @@
+const people = [
+  'Creola Katherine Johnson: mathematician',
+  'Mario José Molina-Pasquel Henríquez: chemist',
+  'Mohammad Abdus Salam: physicist',
+  'Percy Lavon Julian: chemist',
+  'Subrahmanyan Chandrasekhar: astrophysicist'
+];
+
+const listItems = people.map((person)=> <li>{person}</li>)
+
+
 function Greeting() {
-  return <h1>&quot; I swear by my pretty floral bonnet, I will end you. &quot;</h1>
+  return (
+    <>
+      <h1>&quot; I swear by my pretty floral bonnet, I will end you. &quot;</h1>
+      <ul>{listItems}</ul>
+    </>
+  );
 }
 
 export default Greeting;
